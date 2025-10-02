7cd05d47918f4543f0bc2447d0b963867215dfaf
Lucas Machado <lucasmachadoale4@gmail.com>
2025-10-02 16:44:23 -0300

    fix: upgraded the CORS config for frontend

--- api/history.js
+++ api/history.js
@@ -3,8 +3,9 @@ const { getImcHistory } = require('../lib/database');
 module.exports = async function handler(req, res) {
   // Configuração CORS
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+  res.setHeader('Access-Control-Max-Age', '86400');
 
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
